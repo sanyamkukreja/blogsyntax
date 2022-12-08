e666572ede09ec03db6a144685bf48ebdfdb02a5
samir kukreja <samirkukreja1233@gmail.com>
2022-12-08 04:14:10 -0800

    third commit

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Post
 # Create your views here.
 def bloghome(request):
-    return render(request,'blog/bloghome.html')
+    allposts=Post.objects.all()
+    print(f"output--{allposts}")
+    context={'allposts':allposts}
+    return render(request,'blog/bloghome.html',context)
 
 def blogpost(request,slug):
-    return render(request,'blog/blogpost.html')    
+    post=Post.objects.filter(slug=slug).first()
+    print(post)
+    context={'post':post}
+    return render(request,'blog/blogpost.html',context)    
--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,HttpResponse
 from .models import Contact
 from django.contrib import messages
-
+from blog.models import Post
 
 # Create your views here.
 def home(request):
@@ -18,10 +18,18 @@ def contact(request):
         content=request.POST['content']
         print(name,email,phone,content)
         if len(name)<4:
-            messages.error("error text")
+            messages.error(request,"error text")
         else:    
          contact=Contact(name=name,email=email,phone=phone,content=content)
          contact.save()
          messages.success(request,"successful")
 
     return render(request,'home/contact.html') 
+
+def search(request):
+    query=request.GET['query']
+    allposts=Post.objects.filter(title__icontains=query)
+    #allposts=Post.objects.all()
+    params={"allposts":allposts,'query':query}
+
+    return render(request,'home/search.html',params)
